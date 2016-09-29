84fe0b4cc74ef4ba4e4fdc587a92a534798888da
AselaDarshan <aseladarshan@gmail.com>
2016-09-30 00:59:34 +0530

    Adding to github

---
+++ hello_world.sdk/Zync_Design/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../Zync_Design_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Zync_Design_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Zync_Design_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Zync_Design_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../Zync_Design_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Zync_Design_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Zync_Design_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Zync_Design_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
